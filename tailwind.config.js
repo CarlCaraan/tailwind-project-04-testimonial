f073dcd426f28f1fa9d8373887eb26303a893b91
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-12-01 12:11:28 +0800

    12/1/22

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
   content: ["./*.html"],
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
